0bd577182c2e8f0288d93a33888c171f1823e0d7
chintansood <chintansood9@gmail.com>
2026-04-24 02:03:44 +0530

    test next step

--- Backend/Routes/tailorRoutes.js
+++ Backend/Routes/tailorRoutes.js
@@ -1,17 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const tailorCtrl = require("../Controller/Controller_Tailor");
-const findTailorCtrl = require("../Controller/Controller_FindTailor");
-const reviewCtrl = require("../Controller/Controller_Review");
-const auth = require("../Middleware/auth");
 
-router.post("/save-profile",  auth, tailorCtrl.saveOrUpdateTailorProfile);
-router.post("/search",        auth, tailorCtrl.searchTailor);
-router.post("/aadhaar-ocr",   auth, tailorCtrl.doExtractAadhaar);
-
-router.get("/cities",         auth, findTailorCtrl.getDistinctCities);
-router.post("/find",          auth, findTailorCtrl.findTailors);
-
-router.post("/find-by-phone", auth, reviewCtrl.findTailorByPhone);
+router.get("/", (req, res) => {
+  res.json({ status: true, msg: "tailor route working" });
+});
 
 module.exports = router;
