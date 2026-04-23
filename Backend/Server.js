a98a6c275aa37af37e568e7026f75ee27747ee9d
chintansood <chintansood9@gmail.com>
2026-04-24 03:50:47 +0530

    frontend connected to deployed backend

--- Backend/Server.js
+++ Backend/Server.js
@@ -16,6 +16,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://stichfind.vercel.app/",
+    "https://stich-find-dnzk.vercel.app/",
     "https://tailor-nine-henna.vercel.app" 
   ],
   credentials: true
