2c007964294f36729539cb41585b8ac718cdb4b8
chintansood <chintansood9@gmail.com>
2026-04-24 01:53:57 +0530

    test next step

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,43 +3,30 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
-
-const tailorRoutes = require("./Routes/tailorRoutes");
+const { dbconnect } = require("./Config/DBconfig");
 const userRoutes = require("./Routes/User_routes");
+const tailorRoutes = require("./Routes/tailorRoutes");
 const reviewRoutes = require("./Routes/Review_routes");
 const customerRoutes = require("./Routes/customer_prorotes");
-const { dbconnect } = require("./Config/DBconfig");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// fileUpload sabse last me test karna better hai
 app.use(fileUpload());
 
 app.get("/", async (req, res) => {
   try {
     await dbconnect();
-    res.send("Backend running");
+    res.send("WORKING + DB");
   } catch (err) {
     res.status(500).send("DB ERROR: " + err.message);
   }
 });
 
-// ensure DB connected before routes
-app.use(async (req, res, next) => {
-  try {
-    await dbconnect();
-    next();
-  } catch (err) {
-    res.status(500).json({ status: false, msg: "DB connection failed", error: err.message });
-  }
-});
-
-app.use("/tailor", tailorRoutes);
 app.use("/user", userRoutes);
+app.use("/tailor", tailorRoutes);
 app.use("/review", reviewRoutes);
 app.use("/customer", customerRoutes);
 
