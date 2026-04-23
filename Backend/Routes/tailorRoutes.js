21ca047e2a38da59f9a85396b83fd5801325ef47
chintansood <chintansood9@gmail.com>
2026-04-24 02:05:19 +0530

    test next step

--- Backend/Routes/tailorRoutes.js
+++ Backend/Routes/tailorRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../Middleware/auth");
 
 router.get("/", (req, res) => {
   res.json({ status: true, msg: "tailor route working" });
+});
+
+router.get("/test-auth", auth, (req, res) => {
+  res.json({ status: true, msg: "auth working" });
 });
 
 module.exports = router;
