08d15f9234ef7440dce52635ac2aa83480bd6df3
chintansood <chintansood9@gmail.com>
2026-04-24 01:54:44 +0530

    test next step

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const fileUpload = require("express-fileupload");
 const { dbconnect } = require("./Config/DBconfig");
 const userRoutes = require("./Routes/User_routes");
 const tailorRoutes = require("./Routes/tailorRoutes");
@@ -14,7 +13,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(fileUpload());
 
 app.get("/", async (req, res) => {
   try {
