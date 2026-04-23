0082a25ae768add325afa4cc4b446942684c4946
chintansood <chintansood9@gmail.com>
2026-04-24 02:08:36 +0530

    test next step

--- Backend/Routes/tailorRoutes.js
+++ Backend/Routes/tailorRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../Middleware/auth");
-const reviewCtrl = require("../Controller/Controller_Review");
+const tailorCtrl = require("../Controller/Controller_Tailor");
 
 router.get("/", (req, res) => {
   res.json({ status: true, msg: "tailor route working" });
 });
 
-router.post("/find-by-phone", auth, reviewCtrl.findTailorByPhone);
+router.post("/save-profile", auth, tailorCtrl.saveOrUpdateTailorProfile);
 
 module.exports = router;
