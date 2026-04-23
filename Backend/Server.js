3286e9a0688431659342315cdbe66bec7ced5c8a
chintansood <chintansood9@gmail.com>
2026-04-24 01:02:29 +0530

    minimal backend test

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,11 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const { dbconnect } = require("./Config/DBconfig");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+dbconnect();
 
 app.get("/", (req, res) => {
   res.send("WORKING");
